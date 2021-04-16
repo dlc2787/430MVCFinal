7442784bd059822e555a85aed1f409d8630cc6ad
dlc2787 <54549285+dlc2787@users.noreply.github.com>
2021-04-16 18:03:02 -0400

    Port from Domo e
    code base copied from domomaker e to serve as base for mvc final project

---
+++ server/controllers/Domo.js
@@ -0,0 +1,59 @@
+const models = require('../models');
+
+const { Domo } = models;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error ooccured!' });
+    }
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
+  });
+};
+
+const make = (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.height) {
+    return res.status(400).json({ error: 'All fields are requried' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    height: req.body.height,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo exists already!' });
+    }
+    return res.status(400).json({ error: 'An error occurred!' });
+  });
+  return domoPromise;
+};
+
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.json({ domos: docs });
+  });
+};
+
+module.exports = {
+  makerPage,
+  make,
+  getDomos,
+};
